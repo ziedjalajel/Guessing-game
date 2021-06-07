6f741a24ff5a1b26da68ccf326f2452142a1830b
ziedjalajel <zied.jalajel1993@gmail.com>
2021-06-07 21:04:47 +0300

    gg

--- src/App.js
+++ src/App.js
@@ -13,31 +13,30 @@ import Hints from "./Components/Hints"
 
 function App() {
   const [output,setoutput]=useState("Please enter a number")
-  const [userInput,setUserInput] = useState('')
   const [number,setnumber]=useState(Math.floor(Math.random()*(100-1)+1))
   const [attempt,setattempt]=useState(1)
   const [level,setlevel]=useState(2)
   const [hint,setHint]= useState(3)
   
-  const btnClicked = ()=>{
+  const btnClicked = (userInput)=>{
     let Input=parseInt(userInput)
 if(attempt<=5){
     if(Input===number){
       setoutput(`Your number was ${number}, and it's CORRECT ,win again if you can`)
-      setUserInput('')
+  
       setlevel(level+1)
       setnumber(Math.floor(Math.random()*((10**level)-1)+1))
       setattempt(1)
     } else if(Input < number){
        setoutput(`Too low  Dude !`)
-       setUserInput('')
+  
     }else if(Input>number){
        setoutput (`DUDE that's high`)
-       setUserInput('')
+  
     }
     console.log(number)
     setattempt(attempt+1)
-    setUserInput('')
+
   }else {
     setnumber(Math.floor(Math.random()*(100-1)+1))
     setattempt(1)
@@ -53,7 +52,7 @@ if(attempt<=5){
   return (
   <ThemeProvider theme={theme}>
     <GlobalStyle/>
-    <Home click={btnClicked} output={output} userInput={userInput} setUserInput={setUserInput} level={level} attempt={attempt} /> 
+    <Home click={btnClicked} output={output} level={level} attempt={attempt} /> 
     <Hints hint={hint} setHint={setHint} number={number} level={level}/>
     </ThemeProvider>
     
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,8 @@
-
+import {useState} from "react"
 
 const Home = (props) => {
-    
+ 
+  const [userInput,setUserInput] = useState('')
 return(
     <>
     <h1>Guessing Game </h1>
@@ -9,8 +10,12 @@ return(
     <div id="container">
       <p>Level: {props.level-1}</p>
       <p id="outputtext">{props.output} </p>
-      <input type="text" id="userInput" value={props.userInput} onChange={event => props.setUserInput(event.target.value)}/>
-      <button id="btn" onClick={()=>props.click()}>Try ! {6 - props.attempt}</button>
+      <input type="text" id="userInput" value={userInput} onChange={event => setUserInput(event.target.value)}/>
+      <button id="btn" onClick={()=>{
+        props.click(userInput)
+        setUserInput('')
+      }
+    }>Try ! {6 - props.attempt}</button>
     
     </div>
     </>
